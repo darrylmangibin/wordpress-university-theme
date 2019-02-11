61dd78ec1e480c89621627163727b0bc52d94ffe
Darryl <darrylmangibin@gmail.com>
2019-02-12 00:44:53 +0800

    Added static files

---
+++ page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<?php while(have_posts()) : the_post()?>
+	<h2>This is a page</h2>
+   <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
