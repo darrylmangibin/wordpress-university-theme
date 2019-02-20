32d80ebd8119b7debf9313cb729ab91831b2d74b
Darryl <darrylmangibin@gmail.com>
2019-02-20 21:30:11 +0800

    static content and assets

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php 
+    while(have_posts()) {
+        the_post();
+        ?>
+
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        
+        <?php
+    }
+?>
+
+<?php get_footer(); ?>
