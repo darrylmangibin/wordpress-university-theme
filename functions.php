7202bcaa71ea62799beb0bc691fdd7befca85fa6
Darryl <darrylmangibin@gmail.com>
2019-02-21 18:21:05 +0800

    edit default query for the event

--- functions.php
+++ functions.php
@@ -34,3 +34,23 @@ add_action('after_setup_theme', 'univerity_features');
 // }
 
 // add_action('init', 'university_post_type');
+
+function university_adjust_queries($query) {
+    $today = date('Ymd');
+    if(!is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query()) {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        ));
+
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
