5b8050716b71fd387071b9efc039a34a16d92a3a
Darryl <darrylmangibin@gmail.com>
2019-02-26 23:41:22 +0800

    Combining front-end and back-end

--- js/modules/search.js
+++ js/modules/search.js
@@ -42,25 +42,94 @@
 		}
 
 		getResults() {
-			$.when(
-				$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
-				$.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-			)
-			.then((posts, pages) => {
-				var combinedResults = posts[0].concat(pages[0])
-					this.resultsDiv.html(`
-					<h2 class="search-overlay__section-title">General Information</h2>
-					${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general Information matches that search</p>' }
+			$.getJSON(universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val(), (results) => {
+				this.resultsDiv.html(`
+					<div class="row">
+						<div class="one-third">
+							<h2 class=""search-overlay__section-title">General Information</h2>
+							${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general Information matches that search</p>' }
 					
-					${combinedResults.map((item) => {
-						return `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type === 'post' ? ` by:${item.authorName}` : ''}</li>`
-					}).join('')}
-					${combinedResults.length ? '</ul>' : ''}
-					`);
+							${results.generalInfo.map((item) => {
+								return `<li><a href="${item.permalink}">${item.title}</a> ${item.postType === 'post' ? ` by:${item.authorName}` : ''}</li>`
+							}).join('')}
+							${results.generalInfo.length ? '</ul>' : ''}
+						</div>
+
+						<div class="one-third">
+							<h2 class=""search-overlay__section-title">Programs</h2>
+								${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs matches that search<a href="${universityData.root_url}/programs">View all programs<a/></p>` }
+					
+								${results.programs.map((item) => {
+									return `<li><a href="${item.permalink}">${item.title}</a></li>`
+								}).join('')}
+								${results.programs.length ? '</ul>' : ''}
+							<h2 class=""search-overlay__section-title">Professors</h2>
+							${results.professors.length ? '<ul class="professors-cards">' : `<p>No professors matches that search</p>` }
+					
+								${results.professors.map((item) => {
+									return `
+										<li class="professor-card__list-item">
+							        <a class="professor-card" href="${item.permalink}">
+							          <img class="professor-card__image" src="${item.image}">
+							          <span class="professor-card__name">${item.title}</span>
+							        </a>
+							      </li>
+									`
+								}).join('')}
+								${results.professors.length ? '</ul>' : ''}
+						</div>
+
+						<div class="one-third">
+							<h2 class=""search-overlay__section-title">Campuses</h2>
+							${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No general campuses matches that search<a href="${universityData.root_url}/campuses">View all campuses<a/></p>` }
+					
+								${results.campuses.map((item) => {
+									return `<li><a href="${item.permalink}">${item.title}</a></li>`
+								}).join('')}
+								${results.campuses.length ? '</ul>' : ''}
+							<h2 class=""search-overlay__section-title">Events</h2>
+							${results.events.length ? '' : `<p>No general events matches that search<a href="${universityData.root_url}/events">View all events<a/></p>` }
+					
+								${results.events.map((item) => {
+									return `
+										<div class="event-summary">
+										  <a class="event-summary__date t-center" href="${item.permalink}">
+										    <span class="event-summary__month">${item.month}</span>
+										    <span class="event-summary__day">${item.day}</span>  
+										  </a>
+										  <div class="event-summary__content">
+										    <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+										    <p>${item.description}<a href="${item.permalink}" class="nu gray">Learn more</a></p>
+										  </div>
+										</div>
+									`
+								}).join('')}
+						</div>
+					</div>
+				`);
 				this.isSpinnerVisible = false;
-			}, () => {
-				this.resultsDiv.html('<p>Unexpected Error; Please try again</p>')
-			});
+			})
+
+
+			// $.when(
+			// 	$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
+			// 	$.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+			// )
+			// .then((posts, pages) => {
+			// 	var combinedResults = posts[0].concat(pages[0])
+			// 		this.resultsDiv.html(`
+			// 		<h2 class="search-overlay__section-title">General Information</h2>
+			// 		${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general Information matches that search</p>' }
+					
+			// 		${combinedResults.map((item) => {
+			// 			return `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type === 'post' ? ` by:${item.authorName}` : ''}</li>`
+			// 		}).join('')}
+			// 		${combinedResults.length ? '</ul>' : ''}
+			// 		`);
+			// 	this.isSpinnerVisible = false;
+			// }, () => {
+			// 	this.resultsDiv.html('<p>Unexpected Error; Please try again</p>')
+			// });
 		}
 		
 		openOverlay() {
